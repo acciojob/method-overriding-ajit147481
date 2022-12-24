b22f464bdb797fe318608fdb59cfd59b87260abe
ajit147481 <rautajit1234@gmail.com>
2022-12-24 16:27:31 +0530

    a

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -12,14 +12,13 @@ public class Main {
 
         @Override
         public String meth(){
-           // super.meth();
             return "Method is overridden in Extended class B";
         }
     }
 
     public static void main(String[] args) {
-        A a=new A();
-        a.meth();
+        B b=new B();
+        b.meth();
 
         B b1=new B();
         b1.meth();
